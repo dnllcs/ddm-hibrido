9bbb288f3f180d0fa20ee9fbd13c41c6c6194b67
Dnllcs <daniellcs04@gmail.com>
2024-09-10 21:01:19 -0300

    fix: database connection

--- lib/infraestrutura/connection.dart
+++ lib/infraestrutura/connection.dart
@@ -1,15 +1,18 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 class Connection {
   static Database? _database;
 
   static Future<Database> initialize() async {
+    databaseFactory = databaseFactoryFfi;
+    sqfliteFfiInit();
     if (_database != null) return _database!;
 
-    final directory = await getApplicationDocumentsDirectory();
-    final path = join(directory.path, 'vertebrados.db');
+    //final directory = await getApplicationDocumentsDirectory();
+    final path = join(await getDatabasesPath(), 'vertebrados.db');
 
     _database = await openDatabase(
       path,
